3248155101e7e4654a13a637f062404448c54f4f
Vladislav <vlad-taranin@yandex.ru>
2023-01-04 11:22:36 +0300

    final

---
+++ task64/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите натуральное число больше 1:");
+int number = Convert.ToInt32(Console.ReadLine());
+Summ(number);
+
+void Summ (int number)
+{
+    if (number < 0) Console.Write($"{number} не натуральное число");
+    if (number == 0) return;
+    Console.Write("{0,4}", number);
+    Summ (number - 1);
+}
---
+++ task66/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите начальное число M:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите начальное число N:");
+int n = Convert.ToInt32(Console.ReadLine());
+SummNumbers(m, n, 0);
+
+void SummNumbers(int m, int n, int sum)
+{
+    if (m > n)
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}");
+        return;
+    }
+    sum = sum + (m++);
+    SummNumbers(m, n, sum);
+}
